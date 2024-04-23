f7220b5a26738d6f7a455748b58cdbaa8ded0dc5
flavio-valle <flavio.valle2006@gmail.com>
2024-04-22 23:36:43 -0300

    nova estrutura do projeto

--- Fase6Cap8Group/src/br/com/fintech/application/TesteCadastro.java
+++ Fase6Cap8Group/src/br/com/fintech/application/TesteCadastro.java
@@ -1,23 +1,48 @@
-package br.com.fintech;
+package br.com.fintech.application;
 
+import br.com.fintech.dao.*;
+import br.com.fintech.model.*;
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class TesteCadastro {
 
 	public static void main(String[] args) {
-		
+
 		InvestimentoDAO dao = new InvestimentoDAO();
+		Scanner scanner = new Scanner(System.in);
 
 		Investimento investimento = new Investimento();
-		investimento.setDescricaoInvestimento("Google");
-		investimento.setDataInvestimento(Calendar.getInstance());
-		investimento.setValorInvestimento(5000);
-		investimento.setRtnInvestimento(30);
+
+		System.out.print("Digite a descrição do investimento: ");
+		investimento.setDescricaoInvestimento(scanner.nextLine());
+
+		System.out.print("Digite o valor do investimento: ");
+		investimento.setValorInvestimento(Double.parseDouble(scanner.nextLine()));
+
+		System.out.print("Digite o retorno do investimento: ");
+		investimento.setRtnInvestimento(Integer.parseInt(scanner.nextLine()));
+
 		investimento.setContaBancariaInvestimento(1);
-		
-		dao.cadastrar(investimento);
 
-		System.out.println("Cadastrado!");
+		investimento.setDataInvestimento(Calendar.getInstance());
+
+		try {
+			if (investimento.getDescricaoInvestimento() == null || investimento.getDescricaoInvestimento().isEmpty()) {
+				throw new IllegalArgumentException("Descrição do investimento não pode ser vazia.");
+			}
+
+			if (investimento.getValorInvestimento() <= 0) {
+				throw new IllegalArgumentException("Valor do investimento deve ser maior que zero.");
+			}
+
+			dao.cadastrar(investimento);
+
+			System.out.println("Cadastrado!");
+		} catch (Exception e) {
+			System.out.println("Erro ao cadastrar: " + e.getMessage());
+		} finally {
+			scanner.close();
+		}
 	}
-
 }
--- Fase6Cap8Group/src/br/com/fintech/application/TesteListagem.java
+++ Fase6Cap8Group/src/br/com/fintech/application/TesteListagem.java
@@ -1,16 +1,22 @@
-package br.com.fintech;
+package br.com.fintech.application;
 
+import br.com.fintech.model.*;
+import br.com.fintech.dao.*;
 import java.util.List;
 
 public class TesteListagem {
 
 	public static void main(String[] args) {
+		try {
+			InvestimentoDAO dao = new InvestimentoDAO();
 
-		InvestimentoDAO dao = new InvestimentoDAO();
-
-		List<Investimento> lista = dao.listar();
-		for (Investimento item : lista) {
-			System.out.println("investimentoId: " + item.getInvestimentoId() + ", descricaoInvestimento: " + item.getDescricaoInvestimento() + ", dataInvestimento: " + item.getDataInvestimento().getTime() + ", valorInvestimento: " + item.getValorInvestimento() + ", rtnInvestimento: " + item.getRtnInvestimento());
+			List<Investimento> lista = dao.listar();
+			for (Investimento item : lista) {
+				System.out.println("\n" + "Investimento nº:"  + item.getInvestimentoId() + "\n" + "descricaoInvestimento: " + item.getDescricaoInvestimento() + ", dataInvestimento: " + item.getDataInvestimento().getTime() + ", valorInvestimento: " + item.getValorInvestimento() + ", rtnInvestimento: " + item.getRtnInvestimento());
+			}
+		} catch (Exception e) {
+			System.err.println("Houve um erro: " + e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
